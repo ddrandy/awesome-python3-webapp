d8e173935e0fc02ffba0ca2449a6e7e6f5dba1b8
Randy <dddrandy@gmail.com>
2017-09-01 14:14:08 +0800

    finish orm

--- www/orm.py
+++ www/orm.py
@@ -1,13 +1,16 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import asyncio, logging
 
-import asyncio, aiomysql
+import aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s' %sql)
 
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
     __pool = await aiomysql.create_pool(
-        host=kw.get('host','localhost'),
-        port=kw.get('port',3306),
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
         user=kw['user'],
         password=kw['password'],
         db=kw['db'],
@@ -44,6 +47,48 @@ async def execute(sql, args):
             raise
         return affected
 
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+class ModelMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' %(name, tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('    found mapping: %s ==> %s'%(k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    if primaryKey:
+                        raise RuntimeError('Duplicate primary key for field: %s' %k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise RuntimeError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey
+        attrs['__field__'] = fields
+        attrs['__select__'] = 'select `%s`, %s from `%s`' %(primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' %(tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' %(tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' %(tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+
+
 class Model(dict, metaclass=ModelMetaclass):
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
@@ -69,6 +114,52 @@ class Model(dict, metaclass=ModelMetaclass):
                 logging.debug('using default value for %s' %(key, str(value)))
                 setattr(self, key, value)
         return value
+
+    @classmethod
+    async def find(cls, pk):
+        ' find object by primary key. '
+        rs = await select('%s where `%s`=?' %(cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' %rows)
+
+class Field(object):
+    def __init__(self, name,column_type, primary_key, default):
+        self.name=name
+        self.column_type=column_type
+        self.primary_key=primary_key
+        self.default=default
+
+    def __str__(self):
+        return '<%s, %s:%s>' %(self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+
+class BooleanField(Field):
+    def __init__(self, name=None, default=0):
+        super().__init__(name, 'boolean', False, default)
+
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
 from orm import Model, StringField, IntegerField
 
